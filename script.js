0ba93f95c7df22b502c005069a03a1f0abcfa7a2
j-Abban <justice.abban@amalitech.org>
2023-07-30 20:34:32 +0000

    el

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-const slider = document.querySelector('input[name = "theme"]')
+const slider = document.querySelector('input[name = "theme"]');
+slider.addEventListener("change", function(){
+    if (this.checked){
+        document.body.classList.add("dark");
+    }else{
+        document.body.classList.remove("dark");
+    }
+});
